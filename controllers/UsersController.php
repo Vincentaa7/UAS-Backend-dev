a66293c8dda2ca84a9496bbe2e195832af49183c
vincentaa7 <230010009@stikom-bali.ac.id>
2024-06-19 23:43:35 +0800

    add function deleteuser

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -54,5 +54,20 @@ class UsersController
             return json_encode(array("message" => "Failed to update user"));
         }
     }
+
+    public function deleteUser()
+    {
+        $data = json_decode(file_get_contents("php://input"), true);
+        $id = $data['id'];
+        $result = $this->usersService->deleteUser($id);
+        if ($result)
+        {
+            return json_encode(array("message" => "User deleted successfully from the database"));
+        }
+        else
+        {
+            return json_encode(array("message" => "Failed to delete user"));
+        }
+    }
    
 }
